b518df1622a3402958b6599bac8d360a5ac4f624
Alekseikifa <aleksei.kifa@gmail.com>
2026-09-16 11:20:05 +0300

    updeit

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ app = FastAPI(title="Learning Platform API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://learning-platform-frontend-pp1w.onrender.com"],
     allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
 
